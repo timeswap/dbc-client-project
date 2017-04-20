ab4c7019b1703e8698e1298232a8fb1a5095e20e
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-04-19 17:15:39 -0700

    Add null: false for title in films migration

--- rated/db/migrate/20170420000407_create_films.rb
+++ rated/db/migrate/20170420000407_create_films.rb
@@ -1,7 +1,7 @@
 class CreateFilms < ActiveRecord::Migration[5.0]
   def change
     create_table :films do |t|
-      t.string :title
+      t.string :title, null: false
 
       t.timestamps
     end
