3b344d6dc450325c46c1de53b0bc9fbfc5efb799
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-04-20 11:00:18 -0700

    Add associations and validation to review model and tentative first try at the recent method

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,13 @@
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :reviewable
+  belongs_to :reviewable, polymorphic: true
+  has_many :ratings, as: :rateable
+  has_many :comments, as: :commentable
+
+
+  validates :text, presence: true
+
+  def self.recent
+    Review.order_by("created_on").limit(5)
+  end
 end
