7e69e38579790a605323c8d82e121aaccf19eb49
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-04-19 17:14:03 -0700

    Add null false to comment migration for text

--- rated/db/migrate/20170419235603_create_comments.rb
+++ rated/db/migrate/20170419235603_create_comments.rb
@@ -1,7 +1,7 @@
 class CreateComments < ActiveRecord::Migration[5.0]
   def change
     create_table :comments do |t|
-      t.text :text
+      t.text :text, null: false
       t.references :commentable, polymorphic: true
       t.references :user, foreign_key: true
 
