8c14ac523ab66fb96c766f8ce3d41be3e9a9d3e6
Joseph Huang <email": null>
2017-04-20 16:13:38 -0700

    Add seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,7 +11,7 @@ Actor.destroy_all
 Review.destroy_all
 
 Category.create([{title: "Drama"}, {title: "Romance"}, {title:"Action"}, {title:"Fantasy"}, {title:"Animation"}, {title:"Horror"}, {title:"Mystery"}, {title:"Thriller"}])
-Film.create([{title: "Titanic"}, {title: "Akira"}, {title: "Forrest Gump"}, {title: "The Matrix"}, {title: "Spirited Away"}, {title: "Psycho"}, {title: "Memento"}, {title: "The Shining"}, {title: "Your Name."}, {title: "Old Boy"}])
+Film.create([{title: "Titanic"}, {title: "Akira"}, {title: "Forrest Gump"}, {title: "The Matrix"}, {title: "Spirited Away"}, {title: "Psycho"}, {title: "Memento"}, {title: "The Shining"}, {title: "Your Name"}, {title: "Old Boy"}])
 Actor.create([{first_name: "Leonardo", last_name: "DiCaprio"}, {first_name: "Tom", last_name: "Hanks"}, {first_name: "Carrie-Anne", last_name: "Moss"}, {first_name: "Anthony", last_name: "Perkins"}, {first_name: "Janet", last_name: "Leigh"}, {first_name: "Carrie-Anne", last_name: "Moss"}, {first_name: "Guy", last_name: "Pearce"}, {first_name: "Jack", last_name: "Nicholson"}, {first_name: "Jie-tae", last_name: "Yu"}])
 Review.create([{text: "Great Movie"}, {text:"Piece of crap"}, {text:"Just ok"}])
 Film.first.reviews << Review.find(1)
