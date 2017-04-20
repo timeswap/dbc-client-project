4c3036dd56e99e6763379130bccbff1095dd1d3b
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-04-20 11:26:01 -0700

    Add associatoins for film

--- app/models/film.rb
+++ app/models/film.rb
@@ -1,2 +1,9 @@
 class Film < ApplicationRecord
+  has_many :ratings, as: :rateable
+  has_many :reviews, as: :reviewable
+  has_many :comments, as: :commentable
+  has_many :actors_films
+  has_many :actors, through: :actors_films
+  has_many :categories_films
+  has_many :categories, through: :categories_films
 end
