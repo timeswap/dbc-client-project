8bdca2af3d7c9f6f0535cf3548c7a8d3a87d97bd
Joseph Huang <email": null>
2017-04-20 17:20:49 -0700

    Limit top 5 reviews for a category

--- app/models/category.rb
+++ app/models/category.rb
@@ -2,4 +2,8 @@ class Category < ApplicationRecord
   has_many :categories_films, dependent: :destroy
   has_many :films, through: :categories_films
   has_many :reviews, through: :films
+
+  def recent
+    self.reviews.order("created_at DESC").limit(5)
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,10 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-<<<<<<< HEAD
 
-=======
->>>>>>> d554e1e54ca8eab58d31c0a24034cd22290e837d
 User.destroy_all
 Film.destroy_all
 Category.destroy_all
@@ -23,7 +20,7 @@ end
 Category.create!([{title: "Drama"}, {title: "Romance"}, {title:"Action"}, {title:"Fantasy"}, {title:"Animation"}, {title:"Horror"}, {title:"Mystery"}, {title:"Thriller"}])
 Film.create!([{title: "Titanic"}, {title: "Akira"}, {title: "Forrest Gump"}, {title: "The Matrix"}, {title: "Spirited Away"}, {title: "Psycho"}, {title: "Memento"}, {title: "The Shining"}, {title: "Your Name"}, {title: "Old Boy"}])
 Actor.create!([{first_name: "Leonardo", last_name: "DiCaprio"}, {first_name: "Tom", last_name: "Hanks"}, {first_name: "Carrie-Anne", last_name: "Moss"}, {first_name: "Anthony", last_name: "Perkins"}, {first_name: "Janet", last_name: "Leigh"}, {first_name: "Carrie-Anne", last_name: "Moss"}, {first_name: "Guy", last_name: "Pearce"}, {first_name: "Jack", last_name: "Nicholson"}, {first_name: "Jie-tae", last_name: "Yu"}])
-Review.create!([{text: "Great Movie", user: User.first, reviewable: Film.first}, {text:"Piece of crap", user: User.second, reviewable: Film.second}, {text:"Just ok", user: User.third, reviewable: Film.third}])
+Review.create!([{text: "Great Movie", user: User.first, reviewable: Film.first}, {text:"Piece of crap", user: User.second, reviewable: Film.second}, {text:"Just ok", user: User.third, reviewable: Film.third}, {text: "Mind Blown", user: User.first, reviewable: Film.fourth}, {text: "Meh", user: User.first, reviewable: Film.fifth}])
 Film.first.categories << Category.first
 Film.first.categories << Category.second
 
