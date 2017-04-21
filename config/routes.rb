59a12f1fd1dc67e85a3a40fd16b6d895bc64faea
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-04-20 23:44:18 -0700

    Add full_name method to actors and use it; clean up routes for actors

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
 
-  resources :actors do
+  resources :actors, only: [:show] do
     get '/reviews/new', to: 'reviews#actor_new'
     post '/reviews', to: 'reviews#actor_create'
   end
