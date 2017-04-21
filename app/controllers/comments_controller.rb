a3668c5a08a854ac38cf6578eea8007c6271f809
Dillon Arevalo <dillonbarevalo@gmail.com>
2017-04-20 17:32:45 -0700

    Reuse much of reviews to make comments.
    Add routes for both nested and non-nested comments, write the methods for creation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,5 +1,35 @@
 class CommentsController < ApplicationController
 
+  def film_new
+    @film = Film.find(params[:film_id])
+    @comment = @film.comments.new
+  end
 
+  def film_create
+    @film = Film.find(params[:film_id])
+    @comment = @film.comments.new(comment_params)
+    if @comment.save
+      redirect_to @comment
+    else
+      @errors = @comment.errors.full_messages
+      render 'film_new'
+    end
+  end
+
+  def review_new
+    @review = Review.find(params[:review_id])
+    @comment = @review.comments.new
+  end
+
+  def review_create
+    @review = Review.find(params[:review_id])
+    @comment = @review.comments.new(review_params)
+    if @comment.save
+      redirect_to @review
+    else
+      @errors = @comment.errors.full_messages
+      render 'review_new'
+    end
+  end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,9 +15,15 @@ Rails.application.routes.draw do
   resources :films do
     get '/reviews/new', to: 'reviews#film_new'
     post '/reviews', to: 'reviews#film_create'
+    get '/comments/new', to: 'comments#film_new'
+    post '/comments', to: 'comments#film_create'
+
   end
 
-  resources :reviews, except: [:new, :create]
+  resources :reviews, except: [:new, :create] do
+    get '/comments/new', to: 'comments#review_new'
+    post '/comments', to: 'comments#review_create'
+  end
 
   resources :categories
 
