3b4a88ee83742628c509d7e0792cce4ee8fcf620
Govind Rai & Namuun Bayaraa <pair+govindrai.namuunaa@devbootcamp.com>
2017-04-20 18:19:24 -0700

    Add functionality for ratings

--- config/routes.rb
+++ config/routes.rb
@@ -6,12 +6,20 @@ Rails.application.routes.draw do
     post '/reviews', to: 'reviews#actor_create'
   end
 
+
   resources :films do
     get '/reviews/new', to: 'reviews#film_new'
     post '/reviews', to: 'reviews#film_create'
+    get '/ratings/new', to: 'ratings#new_film_rating'
+    post '/ratings', to: 'ratings#create_film_rating'
   end
 
-  resources :reviews, except: [:new, :create]
+  resources :reviews, except: [:new, :create] do
+    get '/ratings/new', to: 'ratings#new_review_rating'
+    post '/ratings', to: 'ratings#create_review_rating'
+  end
+
+  resources :ratings, except: [:new, :create]
   resources :categories
 
   root "films#index"
